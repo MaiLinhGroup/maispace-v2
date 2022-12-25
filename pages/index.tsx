7f796d5230ac635fbc8891f0f23bcc8e0169ee5e
ml <ml@mac.os>
2022-12-26 06:04:36 +0700

    style: :lipstick: style hyperlink

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home({
         <ul className="list-none">
           {allPostsData.map(({ id, date, title }) => (
             <li className="mb-5" key={id}>
-              <Link href={`/posts/${id}`} className="text-blue-700">{title}</Link>
+              <Link href={`/posts/${id}`} className="hover:underline underline-offset-auto text-blue-600 hover:text-blue-800 visited:text-purple-600">{title}</Link>
               <br />
               <small className="text-neutral-500">
                 <Date dateString={date} />
