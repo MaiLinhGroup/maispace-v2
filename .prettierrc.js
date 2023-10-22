23bd2d306f4177bbaaca660cbdd3b561f910d7de
ml <ml@mac.os>
2023-10-23 00:11:36 +0200

    style: :lipstick: use prettier as standard formatter

--- .prettierrc.js
+++
@@ -1,5 +0,0 @@
-export const printWidth = 80;
-export const semi = true;
-export const singleQuote = true;
-export const tabWidth = 2;
-export const useTabs = false;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import { GetStaticProps } from 'next'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { GetStaticProps } from "next";
 
 export default function Home({
-  allPostsData
+  allPostsData,
 }: {
   allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
+    date: string;
+    title: string;
+    id: string;
+  }[];
 }) {
   return (
     <Layout home>
@@ -20,17 +20,22 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className="text-lg">
-        <p>Moin and welcome to MaiSpace, my personal blog and portfolio website.</p>
         <p>
-          I&#39;m Mai Linh and I&#39;m a software engineer.
+          Moin and welcome to MaiSpace, my personal blog and portfolio website.
         </p>
+        <p>I&#39;m Mai Linh and I&#39;m a software engineer.</p>
       </section>
       <section className="text-lg padding-top">
         <h2 className="text-xl font-semibold my-4">Blog</h2>
         <ul className="list-none">
           {allPostsData.map(({ id, date, title }) => (
             <li className="mb-5" key={id}>
-              <Link href={`/posts/${id}`} className="hover:underline underline-offset-auto text-blue-600 hover:text-blue-800">{title}</Link>
+              <Link
+                href={`/posts/${id}`}
+                className="hover:underline underline-offset-auto text-blue-600 hover:text-blue-800"
+              >
+                {title}
+              </Link>
               <br />
               <small className="text-neutral-500">
                 <Date dateString={date} />
@@ -40,14 +45,14 @@ export default function Home({
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
-}
+      allPostsData,
+    },
+  };
+};
