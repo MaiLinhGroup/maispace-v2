d09410e469736e1a6ca35d3111e9ed66a1ed5ab0
ml <ml@mac.os>
2023-11-06 21:13:56 +0100

    style: :lipstick: update meta data

--- components/layout.tsx
+++ components/layout.tsx
@@ -29,16 +29,25 @@ export default function Layout({
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta name="author" content="Chubby Chopstix"></meta>
+        <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="I'm excited to have you here on my personal blog and
+          portfolio website."
         />
         <meta
+          name="keywords"
+          content="TypeScript, Next.js, Tailwind CSS"
+        ></meta>
+        <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="https://www.mailinh.cc/images/profile.jpg"
         />
         <meta name="og:title" content={siteTitle} />
+        <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Header home={home} />
