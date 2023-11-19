874674eeca134d9859079fda35905754169cab6b
ml <ml@mac.os>
2023-11-19 22:42:42 +0100

    Add ThemeSwitch component to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import ThemeSwitch from "./themeswitch";
 
 export default function Header({ home }: { home?: boolean }) {
   const name = "Moin, I'm Mai Linh!";
   const pathname = usePathname();
   return (
     <header className="flex flex-col items-center">
-      <nav className="hover:underline underline-offset-auto text-blue-600 hover:text-blue-800 text-xl font-semibold mb-2">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <nav className="hover:underline underline-offset-auto text-xl font-semibold mb-2">
+        <div className=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div className="w-full md:block md:w-auto" id="navbar-default">
             <ul className="inline-flex items-center justify-between space-x-4">
-              <li>
+              <li className="text-blue-600 hover:text-blue-800">
                 <Link
                   className={`link ${pathname === "/" ? "active" : ""}`}
                   href="/"
@@ -18,7 +19,7 @@ export default function Header({ home }: { home?: boolean }) {
                   Home
                 </Link>
               </li>
-              <li>
+              <li className="text-blue-600 hover:text-blue-800">
                 <Link
                   className={`link ${pathname === "/posts" ? "active" : ""}`}
                   href="/posts"
@@ -26,13 +27,17 @@ export default function Header({ home }: { home?: boolean }) {
                   Posts
                 </Link>
               </li>
-              <li>
+              <li className="text-blue-600 hover:text-blue-800">
                 <Link
                   className={`link ${pathname === "/about" ? "active" : ""}`}
                   href="/about"
                 >
                   About
                 </Link>
+              </li>
+              <li>
+                {" "}
+                <ThemeSwitch />
               </li>
             </ul>
           </div>
@@ -49,7 +54,9 @@ export default function Header({ home }: { home?: boolean }) {
               width={144}
               alt={name}
             />
-            <h1 className="text-4xl font-bold tracking-tighter">{name}</h1>
+            <h1 className="text-4xl font-bold tracking-tighter text-slate-900 dark:text-slate-50">
+              {name}
+            </h1>
           </>
         ) : (
           <>
